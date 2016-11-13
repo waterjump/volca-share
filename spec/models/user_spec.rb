23a4fb46049e45a1ec46b4ce4558154d9f747490
Sean Barrett <sbarrett@weblinc.com>
2016-11-12 21:30:59 -0500

    Add User model spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe User, 'validations' do
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email) }
+  it { is_expected.to validate_presence_of(:username) }
+  it { is_expected.to validate_uniqueness_of(:username) }
+  it do
+    is_expected.to validate_format_of(:username)
+      .to_allow('hotlava69')
+      .not_to_allow("qq")
+      .not_to_allow('webr4%^%$E')
+  end
+  it { is_expected.to validate_length_of(:username).within(3..20) }
+end
