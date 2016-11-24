c373271cba114692feca3ef547d2682d2b25cd44
Sean Barrett <sbarrett@weblinc.com>
2016-11-24 15:19:39 -0500

    DRY up js, views.  Add sync function in patch view.

--- app/assets/javascripts/midi.js
+++ app/assets/javascripts/midi.js
@@ -4,3 +4,44 @@ WebMidi.enable(function(err) {
   if (err) console.log("An error occurred", err);
   midiAvailable = true;
 });
+
+$(function(){
+  if (midiAvailable) {
+    var items = '<option>Midi Device</option>';
+    $(WebMidi.outputs).each(function() {
+      items += '<option value="' + this.name + '">' + this.name + '</option>';
+    });
+    $('#midi-device').html(items);
+  } else {
+    $('#midi-output').hide();
+    $('.knob').removeClass('midi-enabled');
+  }
+
+  $('#midi-device').change(function(){
+    midiOutput = WebMidi.getOutputByName($(this).val());
+    enableSync();
+  });
+
+  $('#midi-channel').change(function(){
+    midiChannel = $(this).val();
+    enableSync();
+  });
+
+  function enableSync(){
+    if (typeof midiOutput !== 'undefined' && typeof midiChannel !== 'undefined') {
+      $('#sync').removeClass('disabled')
+    }
+  }
+
+  $('#sync').click(function(){
+    $('.knob').each(function(){
+      if (typeof midiOutput !== 'undefined' && typeof midiChannel !== 'undefined') {
+          midiOutput.sendControlChange(
+            $(this).data('control-number'),
+            $(this).data('midi'),
+            midiChannel
+          )
+      }
+    });
+  });
+});
