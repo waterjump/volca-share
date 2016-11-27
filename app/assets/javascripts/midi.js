95413cade435a5c9427cae2fc026dddc523b8c7b
Sean Barrett <sbarrett@weblinc.com>
2016-11-26 20:19:53 -0500

    Default to first available midi device and channel.

--- app/assets/javascripts/midi.js
+++ app/assets/javascripts/midi.js
@@ -1,31 +1,36 @@
-var midiAvailable, midiOutput, midiChannel;
+var midiSupported, midiOutput, midiChannel;
 
 WebMidi.enable(function(err) {
   if (err) console.log("An error occurred", err);
-  midiAvailable = true;
+  midiSupported = true;
 });
 
 $(function(){
-  if (midiAvailable && WebMidi.outputs.count > 0) {
+  if (midiSupported && WebMidi.outputs.length > 0) {
     var items = '<option>Midi Device</option>';
+
+    $('#midi-device').change(function(){
+      midiOutput = WebMidi.getOutputByName($(this).val());
+      enableSync();
+    });
+
+    $('#midi-channel').change(function(){
+      midiChannel = $(this).val();
+      enableSync();
+    });
+
     $(WebMidi.outputs).each(function() {
       items += '<option value="' + this.name + '">' + this.name + '</option>';
     });
     $('#midi-device').html(items);
+    $('#midi-device option:eq(1)').attr("selected", "selected");
+    $('#midi-device').trigger('change');
+    $('#midi-channel option:eq(1)').attr("selected", "selected");
+    $('#midi-channel').trigger('change');
   } else {
     $('#midi-output').hide();
     $('.knob').removeClass('midi-enabled');
   }
-
-  $('#midi-device').change(function(){
-    midiOutput = WebMidi.getOutputByName($(this).val());
-    enableSync();
-  });
-
-  $('#midi-channel').change(function(){
-    midiChannel = $(this).val();
-    enableSync();
-  });
 
   function enableSync(){
     if (typeof midiOutput !== 'undefined' && typeof midiChannel !== 'undefined') {
