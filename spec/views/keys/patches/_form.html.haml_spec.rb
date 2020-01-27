e26cebf1f3e03e6df7ae6b6e2d4f9c7633c7699f
Sean Barrett <sean@volcashare.com>
2020-01-27 18:06:40 +0100

    Add tests for keys interface DOM elements
    This is a test to check if a control for the keys interface exists, in
    an attempt to make the form building a little more test-driven.

--- spec/views/keys/patches/_form.html.haml_spec.rb
+++ spec/views/keys/patches/_form.html.haml_spec.rb
@@ -17,6 +17,26 @@ RSpec.describe 'keys/patches/_form.html.haml', type: :view do
     end
   end
 
+  it 'shows controls from the keys machine' do
+    @patch = VolcaShare::Keys::PatchViewModel.wrap(Keys::Patch.new)
+    render
+
+    expect(rendered).to have_css('#voice')
+    expect(rendered).to have_css('#octave')
+    expect(rendered).to have_css('#detune')
+    expect(rendered).to have_css('#portamento')
+    expect(rendered).to have_css('#vco-eg-int')
+    expect(rendered).to have_css('#cutoff')
+    expect(rendered).to have_css('#peak')
+    expect(rendered).to have_css('#vcf-eg-int')
+    expect(rendered).to have_css('#rate')
+    expect(rendered).to have_css('#pitch-int')
+    expect(rendered).to have_css('#cutoff-int')
+    expect(rendered).to have_css('#attack')
+    expect(rendered).to have_css('#decay-release')
+    expect(rendered).to have_css('#sustain')
+  end
+
   xit 'shows tag input placeholders' do
     @patch = VolcaShare::PatchViewModel.wrap(Patch.new)
     render partial: 'keys/patches/form.html.haml'
