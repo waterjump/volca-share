2adfb000d980434036618e7c3c41cf0fb8a889dc
Sean Barrett <sean@volcashare.com>
2020-01-28 19:27:51 +0100

    Add volca keys lfo shape controls
    This involves dummy methods in the Keys::PatchViewModel object because
    radio buttons are a little harder to spoof without values available.
    They will be implemented for real when patch submission is developed.

--- app/view_models/volca_share/keys/patch_view_model.rb
+++ app/view_models/volca_share/keys/patch_view_model.rb
@@ -3,6 +3,20 @@
 module VolcaShare
   module Keys
     class PatchViewModel < ApplicationViewModel
+      def lfo_shape_saw
+        # TODO: This is a stub
+        true
+      end
+
+      def lfo_shape_tri
+        # TODO: This is a stub
+        true
+      end
+
+      def lfo_shape_square
+        # TODO: This is a stub
+        true
+      end
     end
   end
 end
