98b2b3f16646d84d2e775a300be692296a5e6ab0
Sean Barrett <sean@volcashare.com>
2020-02-03 00:24:14 +0100

    Add fields and validations to volca keys model

--- app/models/keys/patch.rb
+++ app/models/keys/patch.rb
@@ -8,6 +8,53 @@ module Keys
     include ActiveModel::Validations
 
     field :name, type: String
+    field :secret, type: Boolean, default: false
+    field :notes, type: String
+    field :slug, type: String
+
+    field :voice, type: Integer, default: 57
+    field :octave, type: Integer, default: 57
+    field :detune, type: Integer, default: 0
+    field :portamento, type: Integer, default: 0
+    field :vco_eg_int, type: Integer, default: 0
+    field :cutoff, type: Integer, default: 63
+    field :peak, type: Integer, default: 0
+    field :vcf_eg_int, type: Integer, default: 0
+    field :lfo_rate, type: Integer, default: 0
+    field :lfo_pitch_int, type: Integer, default: 0
+    field :lfo_cutoff_int, type: Integer, default: 0
+    field :attack, type: Integer, default: 0
+    field :decay_release, type: Integer, default: 63
+    field :sustain, type: Integer, default: 127
+    field :delay_time, type: Integer, default: 0
+    field :delay_feedback, type: Integer, default: 0
+    field :lfo_shape, type: String, default: 'triangle'
+    field :lfo_trigger_sync, type: Boolean, default: false
+    field :step_trigger, type: Boolean, default: false
+    field :tempo_delay, type: Boolean, default: true
+
+    midi_validation_options = {
+      numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 127 }
+    }
+
+    validates_presence_of :name, :slug
+    validates :voice, inclusion: { in: [0, 19, 38, 57, 76, 95] }
+    validates :octave, inclusion: { in: [0, 19, 38, 57, 76, 95] }
+    validates :detune, :only_integer, midi_validation_options
+    validates :portamento, :only_integer, midi_validation_options
+    validates :vco_eg_int, :only_integer, midi_validation_options
+    validates :cutoff, :only_integer, midi_validation_options
+    validates :peak, :only_integer, midi_validation_options
+    validates :vcf_eg_int, :only_integer, midi_validation_options
+    validates :lfo_rate, :only_integer, midi_validation_options
+    validates :lfo_pitch_int, :only_integer, midi_validation_options
+    validates :lfo_cutoff_int, :only_integer, midi_validation_options
+    validates :attack, :only_integer, midi_validation_options
+    validates :decay_release, :only_integer, midi_validation_options
+    validates :sustain, :only_integer, midi_validation_options
+    validates :delay_time, :only_integer, midi_validation_options
+    validates :delay_feedback, :only_integer, midi_validation_options
+    validates :lfo_shape, inclusion: { in: %w(saw triangle square) }
   end
 end
 
---
+++ spec/models/keys/patch_spec.rb
@@ -0,0 +1,279 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Keys::Patch do
+  describe 'fields' do
+    it { is_expected.to have_field(:name).of_type(String) }
+
+    it do
+      is_expected.to(
+        have_field(:secret)
+          .of_type(Mongoid::Boolean)
+          .with_default_value_of(false)
+      )
+    end
+
+    it { is_expected.to have_field(:notes).of_type(String) }
+    it { is_expected.to have_field(:slug).of_type(String) }
+
+    it do
+      is_expected.to(
+        have_field(:voice).of_type(Integer).with_default_value_of(57)
+      )
+    end
+
+    it do
+      is_expected.to(
+        have_field(:octave).of_type(Integer).with_default_value_of(57)
+      )
+    end
+
+    it do
+      is_expected.to(
+        have_field(:detune).of_type(Integer).with_default_value_of(0)
+      )
+    end
+
+    it do
+      is_expected.to(
+        have_field(:portamento).of_type(Integer).with_default_value_of(0)
+      )
+    end
+
+    it do
+      is_expected.to(
+        have_field(:vco_eg_int).of_type(Integer).with_default_value_of(0)
+      )
+    end
+
+    it do
+      is_expected.to(
+        have_field(:cutoff).of_type(Integer).with_default_value_of(63)
+      )
+    end
+
+    it do
+      is_expected.to(
+        have_field(:peak).of_type(Integer).with_default_value_of(0)
+      )
+    end
+
+    it do
+      is_expected.to(
+        have_field(:vcf_eg_int).of_type(Integer).with_default_value_of(0)
+      )
+    end
+
+    it do
+      is_expected.to(
+        have_field(:lfo_rate).of_type(Integer).with_default_value_of(0)
+      )
+    end
+
+    it do
+      is_expected.to(
+        have_field(:lfo_pitch_int).of_type(Integer).with_default_value_of(0)
+      )
+    end
+
+    it do
+      is_expected.to(
+        have_field(:lfo_cutoff_int).of_type(Integer).with_default_value_of(0)
+      )
+    end
+
+    it do
+      is_expected.to(
+        have_field(:attack).of_type(Integer).with_default_value_of(0)
+      )
+    end
+
+    it do
+      is_expected.to(
+        have_field(:decay_release).of_type(Integer).with_default_value_of(63)
+      )
+    end
+
+    it do
+      is_expected.to(
+        have_field(:sustain).of_type(Integer).with_default_value_of(127)
+      )
+    end
+
+    it do
+      is_expected.to(
+        have_field(:delay_time).of_type(Integer).with_default_value_of(0)
+      )
+    end
+
+    it do
+      is_expected.to(
+        have_field(:delay_feedback).of_type(Integer).with_default_value_of(0)
+      )
+    end
+
+    it do
+      is_expected.to(
+        have_field(:lfo_shape).of_type(String).with_default_value_of('triangle')
+      )
+    end
+
+    it do
+      is_expected.to(
+        have_field(:lfo_trigger_sync)
+          .of_type(Mongoid::Boolean)
+          .with_default_value_of(false)
+      )
+    end
+
+    it do
+      is_expected.to(
+        have_field(:step_trigger)
+          .of_type(Mongoid::Boolean)
+          .with_default_value_of(false)
+      )
+    end
+
+    it do
+      is_expected.to(
+        have_field(:tempo_delay)
+          .of_type(Mongoid::Boolean)
+          .with_default_value_of(true)
+      )
+    end
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:slug) }
+
+    it do
+      is_expected.to validate_inclusion_of(:voice).to_allow(0, 19, 38, 57, 76, 95)
+    end
+
+    it do
+      is_expected.to(
+        validate_inclusion_of(:octave).to_allow(0, 19, 38, 57, 76, 95)
+      )
+    end
+
+    it do
+      is_expected.to(
+        validate_numericality_of(:detune)
+          .greater_than_or_equal_to(0)
+          .less_than_or_equal_to(127)
+      )
+    end
+
+    it do
+      is_expected.to(
+        validate_numericality_of(:portamento)
+          .greater_than_or_equal_to(0)
+          .less_than_or_equal_to(127)
+      )
+    end
+
+    it do
+      is_expected.to(
+        validate_numericality_of(:vco_eg_int)
+          .greater_than_or_equal_to(0)
+          .less_than_or_equal_to(127)
+      )
+    end
+
+    it do
+      is_expected.to(
+        validate_numericality_of(:cutoff)
+          .greater_than_or_equal_to(0)
+          .less_than_or_equal_to(127)
+      )
+    end
+
+    it do
+      is_expected.to(
+        validate_numericality_of(:peak)
+          .greater_than_or_equal_to(0)
+          .less_than_or_equal_to(127)
+      )
+    end
+
+    it do
+      is_expected.to(
+        validate_numericality_of(:vcf_eg_int)
+          .greater_than_or_equal_to(0)
+          .less_than_or_equal_to(127)
+      )
+    end
+
+    it do
+      is_expected.to(
+        validate_numericality_of(:lfo_rate)
+          .greater_than_or_equal_to(0)
+          .less_than_or_equal_to(127)
+      )
+    end
+
+    it do
+      is_expected.to(
+        validate_numericality_of(:lfo_pitch_int)
+          .greater_than_or_equal_to(0)
+          .less_than_or_equal_to(127)
+      )
+    end
+
+    it do
+      is_expected.to(
+        validate_numericality_of(:lfo_cutoff_int)
+          .greater_than_or_equal_to(0)
+          .less_than_or_equal_to(127)
+      )
+    end
+
+    it do
+      is_expected.to(
+        validate_numericality_of(:attack)
+          .greater_than_or_equal_to(0)
+          .less_than_or_equal_to(127)
+      )
+    end
+
+    it do
+      is_expected.to(
+        validate_numericality_of(:decay_release)
+          .greater_than_or_equal_to(0)
+          .less_than_or_equal_to(127)
+      )
+    end
+
+    it do
+      is_expected.to(
+        validate_numericality_of(:sustain)
+          .greater_than_or_equal_to(0)
+          .less_than_or_equal_to(127)
+      )
+    end
+
+    it do
+      is_expected.to(
+        validate_numericality_of(:delay_time)
+          .greater_than_or_equal_to(0)
+          .less_than_or_equal_to(127)
+      )
+    end
+
+    it do
+      is_expected.to(
+        validate_numericality_of(:delay_feedback)
+          .greater_than_or_equal_to(0)
+          .less_than_or_equal_to(127)
+      )
+    end
+
+    it do
+      is_expected.to(
+        validate_inclusion_of(:lfo_shape).to_allow('saw', 'triangle', 'square')
+      )
+    end
+  end
+end
