75fadee02225dbde5b9b58f937e0587db21b2fdb
Sean Barrett <sean@volcashare.com>
2020-02-10 22:22:05 +0100

    Add feature spec for submitting anonymous keys patch

--- spec/features/keys/patch_submission_spec.rb
+++ spec/features/keys/patch_submission_spec.rb
@@ -26,10 +26,28 @@ RSpec.describe 'Creating a keys patch', type: :feature, js: true do
       )
     end
 
-    it 'directs user to patch show page' do
+    it 'directs user to user patch show page' do
       expect(current_path).to(
         eq(user_keys_patch_path(user.slug, Keys::Patch.first.slug))
       )
+    end
+  end
+
+  context 'when user is not logged in' do
+    before do
+      visit new_keys_patch_path
+      fill_out_keys_patch_form(dummy_patch)
+      click_button 'Save'
+    end
+
+    it 'persists patch' do
+      expect(Keys::Patch.first.attributes).to include(
+        dummy_patch.attributes.except('_id')
+      )
+    end
+
+    it 'directs user to user patch show page' do
+      expect(current_path).to eq(keys_patch_path(Keys::Patch.first.id))
     end
   end
 end
