60aa92c447062be2c1b53c63f4b68603c6cf4228
Sean Barrett <sean@volcashare.com>
2020-02-16 17:17:14 +0100

    Add edit link to keys patch show view

--- spec/views/keys/patches/show.html.haml_spec.rb
+++ spec/views/keys/patches/show.html.haml_spec.rb
@@ -19,10 +19,6 @@ RSpec.describe 'keys/patches/show.html.haml', type: :view do
       render
     end
 
-    it 'reflects the patch' do
-      reflects_keys_patch(@patch, interface: rendered)
-    end
-
     it 'includes h1 header' do
       expect(rendered).to have_selector(
         'h1',
@@ -33,6 +29,71 @@ RSpec.describe 'keys/patches/show.html.haml', type: :view do
 
     it 'displays author name' do
       expect(rendered).to have_content("by #{@patch.user.username}")
+    end
+
+    it 'does not show edit link' do
+      expect(rendered).not_to have_link('Edit')
+    end
+  end
+
+  context 'when user is logged in' do
+    before { @patch = user_patch }
+
+    context 'when user is author' do
+      before do
+        render(
+          template: 'keys/patches/show.html.haml',
+          locals: { current_user: user }
+        )
+      end
+
+      it 'shows edit link' do
+        expect(rendered).to have_link('Edit')
+      end
+    end
+
+    context 'when user is not the author' do
+      before do
+        render(
+          template: 'keys/patches/show.html.haml',
+          locals: { current_user: create(:user) }
+        )
+      end
+
+      it 'does not show edit link' do
+        expect(rendered).not_to have_link('Edit')
+      end
+    end
+  end
+
+  context 'when patch author is anonymous' do
+    before do
+      @patch = anonymous_patch
+
+      render(
+        template: 'keys/patches/show.html.haml',
+        locals: { current_user: nil }
+      )
+    end
+
+    it 'has h1 for SEO purposes' do
+      expect(rendered).to have_selector(
+        'h1',
+        text: "#{@patch.name} by ¯\\_(ツ)_/¯",
+        visible: false
+      )
+    end
+
+    it 'does not show edit link' do
+      expect(rendered).not_to have_link('Edit')
+    end
+
+    it 'shows the volca interface' do
+      expect(rendered).to have_css('.volca.keys')
+    end
+
+    it 'shows the shruggy' do
+      expect(rendered).to have_content('by ¯\_(ツ)_/¯')
     end
   end
 end
