5044c34a28285b94fde870c6e33c9a6cdfc8669a
Sean Barrett <sean@volcashare.com>
2020-05-21 11:43:06 +0200

    Add audio_sample_available field to bass patch model

--- spec/models/patch_spec.rb
+++ spec/models/patch_spec.rb
@@ -2,67 +2,79 @@
 
 require 'rails_helper'
 
-RSpec.describe Patch, 'validations' do
-  it { is_expected.to validate_presence_of(:name) }
-  it { is_expected.to validate_uniqueness_of(:name) }
-  it { is_expected.to validate_presence_of(:slug) }
-  it { is_expected.to validate_uniqueness_of(:slug) }
-  it { is_expected.to validate_numericality_of(:attack) }
-  it { is_expected.to validate_numericality_of(:decay_release) }
-  it { is_expected.to validate_numericality_of(:cutoff_eg_int) }
-  it { is_expected.to validate_numericality_of(:peak) }
-  it { is_expected.to validate_numericality_of(:cutoff) }
-  it { is_expected.to validate_numericality_of(:lfo_rate) }
-  it { is_expected.to validate_numericality_of(:lfo_int) }
-  it { is_expected.to validate_numericality_of(:vco1_pitch) }
-  it { is_expected.to validate_numericality_of(:vco2_pitch) }
-  it { is_expected.to validate_numericality_of(:vco3_pitch) }
-  it do
-    is_expected.to(
-      custom_validate(:audio_sample).with_validator(AudioSampleValidator)
-    )
-  end
-
-  describe '#patch_is_not_default' do
-    context 'when all patch synth paramaters are default' do
-      it 'marks the patch as invalid' do
-        default_patch =
-          FactoryBot.build(
-            :patch,
-            attack: 63,
-            decay_release: 63,
-            cutoff_eg_int: 63,
-            octave: 63,
-            peak: 63,
-            cutoff: 63,
-            lfo_rate: 63,
-            lfo_int: 63,
-            vco1_pitch: 63,
-            vco1_active: true,
-            vco2_pitch: 63,
-            vco2_active: true,
-            vco3_pitch: 63,
-            vco3_active: true,
-            vco_group: 'three',
-            lfo_target_amp: false,
-            lfo_target_pitch: false,
-            lfo_target_cutoff: true,
-            lfo_wave: false,
-            vco1_wave: false,
-            vco2_wave: false,
-            vco3_wave: true,
-            sustain_on: false,
-            amp_eg_on: false
-          )
-        expect(default_patch).to be_invalid
-      end
+RSpec.describe Patch  do
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name) }
+    it { is_expected.to validate_presence_of(:slug) }
+    it { is_expected.to validate_uniqueness_of(:slug) }
+    it { is_expected.to validate_numericality_of(:attack) }
+    it { is_expected.to validate_numericality_of(:decay_release) }
+    it { is_expected.to validate_numericality_of(:cutoff_eg_int) }
+    it { is_expected.to validate_numericality_of(:peak) }
+    it { is_expected.to validate_numericality_of(:cutoff) }
+    it { is_expected.to validate_numericality_of(:lfo_rate) }
+    it { is_expected.to validate_numericality_of(:lfo_int) }
+    it { is_expected.to validate_numericality_of(:vco1_pitch) }
+    it { is_expected.to validate_numericality_of(:vco2_pitch) }
+    it { is_expected.to validate_numericality_of(:vco3_pitch) }
+    it do
+      is_expected.to(
+        custom_validate(:audio_sample).with_validator(AudioSampleValidator)
+      )
     end
 
-    context 'when patch is not default' do
-      it 'validates the patch' do
-        non_default_patch = FactoryBot.build(:patch, peak: 69)
-        expect(non_default_patch).to be_valid
+    describe '#patch_is_not_default' do
+      context 'when all patch synth paramaters are default' do
+        it 'marks the patch as invalid' do
+          default_patch =
+            FactoryBot.build(
+              :patch,
+              attack: 63,
+              decay_release: 63,
+              cutoff_eg_int: 63,
+              octave: 63,
+              peak: 63,
+              cutoff: 63,
+              lfo_rate: 63,
+              lfo_int: 63,
+              vco1_pitch: 63,
+              vco1_active: true,
+              vco2_pitch: 63,
+              vco2_active: true,
+              vco3_pitch: 63,
+              vco3_active: true,
+              vco_group: 'three',
+              lfo_target_amp: false,
+              lfo_target_pitch: false,
+              lfo_target_cutoff: true,
+              lfo_wave: false,
+              vco1_wave: false,
+              vco2_wave: false,
+              vco3_wave: true,
+              sustain_on: false,
+              amp_eg_on: false
+            )
+          expect(default_patch).to be_invalid
+        end
       end
+
+      context 'when patch is not default' do
+        it 'validates the patch' do
+          non_default_patch = FactoryBot.build(:patch, peak: 69)
+          expect(non_default_patch).to be_valid
+        end
+      end
+    end
+  end
+
+  describe 'fields' do
+    it do
+      is_expected.to(
+        have_field(:audio_sample_available)
+          .of_type(Mongoid::Boolean)
+          .with_default_value_of(nil)
+      )
     end
   end
 
