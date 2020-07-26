fcdd7a69de5555a2ce5b4c300a84a4045d6d6b92
Sean Barrett <sean@volcashare.com>
2020-07-26 13:11:34 +0200

    Remove debugging command from simulation test

--- spec/features/simulation_spec.rb
+++ spec/features/simulation_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe 'Volca Bass Emulation', type: :feature  do
   it 'has its own page' do
     visit new_simulation_path
 
-    save_and_open_page
     expect(page).to have_content('Simulator')
     expect(page).to have_css('.volca.bass.simulation')
   end
