c864c944c698837c57f3f74c01bd77e296ce8472
Sean Barrett <sean@volcashare.com>
2020-07-31 17:06:43 +0200

    Initialize simulation with open filter and no peak

--- app/controllers/simulations_controller.rb
+++ app/controllers/simulations_controller.rb
@@ -3,7 +3,9 @@
 class SimulationsController < ApplicationController
   def new
     @body_class = :form
-    @patch = VolcaShare::PatchViewModel.wrap(Patch.new)
+    @patch = VolcaShare::PatchViewModel.wrap(
+      Patch.new(peak: 0, cutoff: 127)
+    )
     @title = 'New Patch'
   end
 end
