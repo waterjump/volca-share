c5471e62e8b8a0ca16602e094afe826981ab6e7a
Sean Barrett <sean@volcashare.com>
2020-08-02 23:05:13 +0200

    Initialize emulator with a few parameters all the way down
    - EG attack
    - Cutoff EG INT
    - LFO rate
    - LFO INT

--- app/controllers/emulators_controller.rb
+++ app/controllers/emulators_controller.rb
@@ -4,7 +4,15 @@ class EmulatorsController < ApplicationController
   def new
     @body_class = :form
     @patch = VolcaShare::PatchViewModel.wrap(
-      Patch.new(peak: 0, cutoff: 127)
+      Patch.new(
+        attack: 0,
+        decay_release: 0,
+        cutoff_eg_int: 0,
+        peak: 0,
+        cutoff: 127,
+        lfo_rate: 0,
+        lfo_int: 0
+      )
     )
     @title = 'Volca Bass Emulator'
   end
