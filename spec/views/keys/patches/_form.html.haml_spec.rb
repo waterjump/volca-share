ceb9c1b718fd6880885e7c15f4f93b4cd94558c5
Sean Barrett <sean@volcashare.com>
2020-08-23 15:34:27 +0200

    Add copy to encourage people to fill out patch more thoroughly

--- spec/views/keys/patches/_form.html.haml_spec.rb
+++ spec/views/keys/patches/_form.html.haml_spec.rb
@@ -3,16 +3,31 @@
 require 'rails_helper'
 
 RSpec.describe 'keys/patches/_form.html.haml', type: :view do
-  context 'when patch has an audio sample' do
-    let!(:user) { FactoryBot.build(:user) }
+  context 'when user is logged in' do
+    let(:user) { build(:user) }
+    let(:locals) { { current_user: user } }
     let(:user_patch) do
-      VolcaShare::Keys::PatchViewModel.wrap(create(:user_keys_patch))
+      VolcaShare::Keys::PatchViewModel.wrap(create(:user_keys_patch, user: user))
     end
 
-    it 'shows a preview of the audio sample' do
+    before do
       @patch = user_patch
-      render partial: 'keys/patches/form.html.haml', locals: { current_user: user }
-      expect(rendered).to have_css('.sample')
+      render partial: 'keys/patches/form.html.haml', locals: locals
+    end
+
+    context 'when patch has an audio sample' do
+      it 'shows a preview of the audio sample' do
+        expect(rendered).to have_css('.sample')
+      end
+    end
+
+    it 'tells how to rank higher on browse page' do
+      expect(rendered).to(
+        have_content(
+          'Note: Giving your patch tags, a description, and especially an '\
+          'audio sample will help it rank higher on browse pages.'
+        )
+      )
     end
   end
 
--- spec/views/patches/_form.html.haml_spec.rb
+++ spec/views/patches/_form.html.haml_spec.rb
@@ -3,18 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe 'patches/_form.html.haml', type: :view do
-  context 'when patch has an audio sample' do
-    let!(:user) { build(:user) }
-    let(:user_patch) do
-      VolcaShare::PatchViewModel.wrap(create(:user_patch, user: user))
-    end
-
-    it 'shows a preview of the audio sample' do
-      @patch = user_patch
-      render partial: 'patches/form.html.haml', locals: { current_user: user }
-      expect(rendered).to have_css('.sample')
-    end
-  end
+  let(:locals) { { } }
 
   it 'shows tag input placeholders' do
     @patch = VolcaShare::PatchViewModel.wrap(Patch.new)
@@ -22,5 +11,43 @@ RSpec.describe 'patches/_form.html.haml', type: :view do
     expect(rendered).to have_selector(
       'input[placeholder="tags, separated, by, commas"]'
     )
+  end
+
+  context 'when user is logged in' do
+    let(:user) { build(:user) }
+    let(:locals) { { current_user: user } }
+    let(:user_patch) do
+      VolcaShare::PatchViewModel.wrap(create(:user_patch, user: user))
+    end
+
+    before do
+      @patch = user_patch
+      render partial: 'patches/form.html.haml', locals: locals
+    end
+
+    context 'when patch has an audio sample' do
+      it 'shows a preview of the audio sample' do
+        expect(rendered).to have_css('.sample')
+      end
+    end
+
+    it 'tells how to rank higher on browse page' do
+      expect(rendered).to(
+        have_content(
+          'Note: Giving your patch tags, a description, and especially an '\
+          'audio sample will help it rank higher on browse pages.'
+        )
+      )
+    end
+  end
+
+  context 'when user is not logged in' do
+    it 'shows a link to create account' do
+      @patch = VolcaShare::PatchViewModel.wrap(Patch.new)
+
+      render partial: 'patches/form.html.haml', locals: locals
+
+      expect(rendered).to have_link('create an account')
+    end
   end
 end
