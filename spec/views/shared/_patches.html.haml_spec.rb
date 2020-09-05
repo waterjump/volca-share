1203769eebb823b87509c2ce2090e60464b2ccff
Sean Barrett <sean@volcashare.com>
2020-09-05 14:00:46 +0200

    Enable sort controls to take audio only filter into account

--- spec/views/shared/_patches.html.haml_spec.rb
+++ spec/views/shared/_patches.html.haml_spec.rb
@@ -4,15 +4,18 @@ require 'rails_helper'
 
 RSpec.describe 'shared/_patches.html.haml', type: :view do
   let(:user) { create(:user) }
+  let(:sort) { :quality }
+  let(:params) { {} }
 
   let(:render_options) do
     {
       partial: 'shared/patches.html.haml',
-      locals: { current_user: nil }
+      locals: { current_user: nil, params: params }
     }
   end
 
   before do
+    @sort = sort
     @patches = Kaminari.paginate_array(
       VolcaShare::PatchViewModel.wrap(patches)
     ).page(1)
@@ -25,6 +28,51 @@ RSpec.describe 'shared/_patches.html.haml', type: :view do
 
     it 'shows a message' do
       expect(rendered).to have_content('No patches to show')
+    end
+  end
+
+  context 'when audio only filter is on' do
+    let(:patches) { [create(:patch)] }
+    let(:params) { { audio_only: 'true' } }
+
+    describe 'date created sort' do
+      it 'keeps audio_only parameter' do
+        expect(rendered).to(
+          have_css('a[href="/patches?audio_only=true&sort=newest"]')
+        )
+      end
+    end
+
+    describe 'quality sort' do
+      let(:sort) { :created_at }
+
+      it 'keeps audio_only parameter' do
+        expect(rendered).to(
+          have_css('a[href="/patches?audio_only=true"]')
+        )
+      end
+    end
+  end
+
+  context 'when audio only filter is off' do
+    let(:patches) { [create(:patch)] }
+
+    describe 'date created sort' do
+      it 'does not have audio_only parameter' do
+        expect(rendered).to(
+          have_css('a[href="/patches?sort=newest"]')
+        )
+      end
+    end
+
+    describe 'quality sort' do
+      let(:sort) { :created_at }
+
+      it 'does not have audio_only parameter' do
+        expect(rendered).to(
+          have_css('a[href="/patches"]')
+        )
+      end
     end
   end
 end
