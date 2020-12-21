637fcd43ac594ec17638e9be3c92ce29f8bb24ce
Sean Barrett <sean@volcashare.com>
2020-12-21 18:33:27 +0100

    Add a link to new bass patch form with generated synth patch name

--- spec/features/synth_patch_namer_spec.rb
+++ spec/features/synth_patch_namer_spec.rb
@@ -2,18 +2,40 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Synth patch namer', type: :feature do
+RSpec.describe 'Synth patch namer', type: :feature, js: true do
   it 'has its own page' do
     visit synth_patch_namer_path
 
     expect(page).to have_selector('h1', text: 'Synth Patch Namer')
   end
 
-  it 'shows a generated patch name', js: true do
+  it 'shows a generated patch name' do
     visit synth_patch_namer_path
 
     click_link 'Gimme a patch name'
 
     expect(find('#name').text).not_to be_blank
+  end
+
+  describe 'creating a bass patch with the generated name' do
+    before do
+      visit synth_patch_namer_path
+
+      click_link 'Gimme a patch name'
+    end
+
+    it 'leads to the new bass patch page' do
+      click_link 'Make a bass patch with this name'
+
+      expect(current_path).to eq(new_patch_path)
+    end
+
+    it 'prefills the patch name field with generated name' do
+      patch_name = find('#name').text
+
+      click_link 'Make a bass patch with this name'
+
+      expect(find('#patch_name').value).to eq(patch_name)
+    end
   end
 end
