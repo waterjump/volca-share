243cbd10328a3c19cd25a9c5edbbf51b2ffd7fee
Sean Barrett <sean@volcashare.com>
2026-04-09 11:59:10 +0200

    Remove /posts endpoint fail2ban rule
    This was causing problems for actual users and there was no actual
    evidence of abuse for this endpoint so away it goes.

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,15 +1,5 @@
 # frozen_string_literal: true
 
-# Block repeated POSTs to /users.
-# After 3 requests in 1 day, block all requests from that IP for 1 week.
-unless Rails.env.test?
-  Rack::Attack.blocklist('fail2ban users post spam') do |req|
-    Rack::Attack::Fail2Ban.filter("users-post-spam-#{req.ip}", maxretry: 3, findtime: 1.day, bantime: 1.week) do
-      req.path == '/users' && req.post?
-    end
-  end
-end
-
 # Block suspicious requests for wordpress specific paths.
 # After 3 blocked requests in 10 minutes, block all requests from that IP for 1 day.
 Rack::Attack.blocklist('fail2ban pentesters') do |req|
